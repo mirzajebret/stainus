65cb74abcd3bea52a53a4a0aeeecfc5bf76f1e48
Mirza Alby Assidiqie <94514652+mirzajebret@users.noreply.github.com>
2026-08-18 18:46:02 +0700

    Update CampusLife.tsx

--- src/components/home/CampusLife.tsx
+++ src/components/home/CampusLife.tsx
@@ -7,7 +7,7 @@ import ImageCard from '../common/ImageCard';
 const campusImages = [
   { src: "images/gedung-utama-kampus.png", caption: "Gedung Utama Kampus" },
   { src: "images/hero-bg-mahasiswa.png", caption: "Diskusi Mahasiswa" },
-  { src: "fotbar_staff.png", caption: "Staff Kampus" }
+  { src: "images/fotbar_staff.png", caption: "Staff Kampus" }
 
 ];
 
