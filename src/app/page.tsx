13a731c8b3d976320107a2d6a01c4bcaed81d3fe
Mirza Alby Assidiqie <94514652+mirzajebret@users.noreply.github.com>
2026-08-18 19:00:52 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function HomePage() {
   return (
     <>
       <Hero />
-      <Stats />
+      {/* <Stats /> */}
       <ChairmanWelcome />
       <ProgramsPreview />
       <CampusLife />
